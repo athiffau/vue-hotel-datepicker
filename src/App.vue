22129415718462919c008b6b01f449ec7dfe7436
Alain Thiffault <athiffau@effectivemomentum.com>
2019-02-01 13:23:08 -0500

    Date labels

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,6 @@
         <h3>Allow check-in and check-out on the same day</h3>
         <DatePicker
           :minNights="0"
-          :textDates="[{'date':'2019-01-31','text':'<br>100€'}]"
         />
       </div>
       <div class="box">
